b15c3f4f4ddbd518cde1181f569808b9f6396d03
cajacko <charlie@Charless-MacBook-Pro.local>
2016-05-01 15:55:51 +0100

    Added bunting

--- routes/index.php
+++ routes/index.php
@@ -1,7 +1,38 @@
 <?php
 
+$bunting = array();
+    
+$count = 1;
+
+while($count < 100) {
+  $bunting_item = array('colour' => '', 'dots' => array());
+    
+  if( $count % 5 == 0 ) {
+    $colour = 1;
+  } elseif( $count % 4 == 0 ) {
+    $colour = 2;
+  } elseif( $count % 3 == 0 ) {
+    $colour = 3;
+  } elseif( $count % 2 == 0 ) {
+    $colour = 4;
+  } else {
+    $colour = 5;
+  }
+
+  $bunting_item['colour'] = $colour;
+    
+  for($i = 1; $i < 9; $i++) {
+    $bunting_item['dots'][] = $i;
+  }
+
+  $bunting[] = $bunting_item;
+  
+  $count++;
+}
+
+
 $template = $twig->loadTemplate('templates/home.twig');
 
 // gzip compress content
 ob_start("ob_gzhandler");
-  echo $template->render(array('config' => $config));
+  echo $template->render(array('config' => $config, 'bunting' => $bunting));
