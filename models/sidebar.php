50233df7b4ed4ff2136eb8ecd052d646371c4cea
cajacko <charlie@Charless-MacBook-Pro.local>
2016-05-05 20:50:42 +0100

    Added smaller default sidebar images

--- models/sidebar.php
+++ models/sidebar.php
@@ -2,8 +2,8 @@
 
 $sidebar = array(
     'profile' => array(
-        array('src' => 'http://vikibell.com/wp-content/uploads/2016/04/IMG_9334.jpg', 'alt' => 'Alt text'),
-        array('src' => 'http://vikibell.com/wp-content/uploads/2016/04/IMG_9334.jpg', 'alt' => 'Alt text'),
-        array('src' => 'http://vikibell.com/wp-content/uploads/2016/04/IMG_9334.jpg', 'alt' => 'Alt text'),
+        array('src' => 'http://vikibell.com/wp-content/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
+        array('src' => 'http://vikibell.com/wp-content/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
+        array('src' => 'http://vikibell.com/wp-content/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
     ),
 );
