23a12e9719402d493712eccba8455bad95581052
cajacko <charlie@Charless-MacBook-Pro.local>
2016-05-06 07:55:38 +0100

    Made sidebar images use can

--- models/sidebar.php
+++ models/sidebar.php
@@ -2,8 +2,8 @@
 
 $sidebar = array(
     'profile' => array(
-        array('src' => 'http://vikibell.com/wp-content/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
-        array('src' => 'http://vikibell.com/wp-content/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
-        array('src' => 'http://vikibell.com/wp-content/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
+        array('src' => $static_uploads . '/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
+        array('src' => $static_uploads . '/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
+        array('src' => $static_uploads . '/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
     ),
 );
