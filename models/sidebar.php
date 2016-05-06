09a17c227f802e9c6de2d31ad3ec1000ed1af257
Charlie Jackson <charlie@charliejackson.com>
2016-05-06 09:08:35 +0100

    Fixed typo

--- models/sidebar.php
+++ models/sidebar.php
@@ -2,7 +2,7 @@
 
 $sidebar = array(
     'profile' => array(
-        array('src' => $static_uploads . '/uploads/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
+        array('src' => $static_uploads . '/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
         array('src' => $static_uploads . '/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
         array('src' => $static_uploads . '/2016/05/IMG_8088-400x300.jpg', 'alt' => 'Alt text'),
     ),
