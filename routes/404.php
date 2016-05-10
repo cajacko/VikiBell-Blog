cc2bf36c5506ead8cf7edac48b8959d2318c9de0
Charlie Jackson <charlie@charliejackson.com>
2016-05-10 15:49:35 +0100

    Basic 404 page styling

--- routes/404.php
+++ routes/404.php
@@ -6,6 +6,12 @@
 $header_code = 404;
 $template_path .= '404';
 
+$vars['breadcrumbs'] = array(
+  'Home' => '/',
+);
+
+$vars['pageTitle'] = '404: Page not found';
+
 page_meta(array(
   'title' => '404 | Viki Bell',
   'description' => 'Looks like Viki can\'t find what your looking for :s',
